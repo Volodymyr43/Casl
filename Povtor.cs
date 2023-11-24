ef7c6758bb65232571ea2e4a449a77f15630d19b
Volodymyr43 <149713203+Volodymyr43@users.noreply.github.com>
2023-11-24 15:42:29 +0200

    Update Povtor.cs

--- Povtor.cs
+++ Povtor.cs
@@ -3,23 +3,23 @@ public class Test
 {
  public static int getNumber(int x)
  {
-		int i = 1;
-		int n = 0;
-		int res;
-		while (i <= 1000 & n != x)
-		{
-      res = i%2;
-			  if (res !=0)
-			  {
-			  Console.WriteLine(i);
-		    n++;
-			  }
-		    i++;
-		 }
+   int i = 1;
+   int n = 0;
+   int res;
+   while (i <= 1000 & n != x)
+   {
+     res = i%2;
+     if (res !=0)
+     {
+     Console.WriteLine(i);
+     n++;
+     }
+   i++;
+   }
   return i;
-	}
-   public static void Main()
-  {
+}
+public static void Main()
+{
   int x = int.Parse(Console.ReadLine());
   getNumber(x);
   }
